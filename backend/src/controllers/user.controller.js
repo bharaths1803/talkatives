584918b42852723e124562c50f0e6d84cc31be76
bharaths1803 <bharaths14051803@gmail.com>
2025-01-07 20:59:35 +0530

    Completed user schema design and signup controller in backend

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: `All fields are required` });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: `Password must contain at least 6 characters` });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: `User already exists` });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ user: newUser });
+  } catch (error) {
+    console.log(`Error in signup controller ${error}`);
+    res.status(500).json({ message: `Internal server error` });
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./utils/db.js"
+import userRoutes from "./routes/user.route.js"
+
 
 
 const app = express()
 dotenv.config()
-
-
 const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.use("/api/users", userRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
