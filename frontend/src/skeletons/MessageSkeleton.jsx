0893c47db185a9e8791574959be92910c796957f
bharaths1803 <bharaths14051803@gmail.com>
2025-01-19 14:55:52 +0530

    Checking merge conflicts

--- frontend/src/skeletons/MessageSkeleton.jsx
+++ frontend/src/skeletons/MessageSkeleton.jsx
@@ -1,7 +1,250 @@
-import React from "react";
-
 const MessageSkeleton = () => {
-  return <div>MessageSkeleton</div>;
+  const dummyArray = new Array(3).fill(null);
+  return (
+    <>
+      {dummyArray.map((_, idx) => (
+        <div key={idx} className="w-full flex justify-end space-x-2 p-2">
+          <div className="bg-skeleton-purple p-3 rounded-lg rounded-tr-none max-w-full"></div>
+          <div className="size-7"></div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default MessageSkeleton;
+
+const Temp = () => {
+  return (
+    <div className="bg-chat-purple h-5/6 overflow-y-auto p-3 px-5">
+      <div className="w-full flex justify-end space-x-2 p-2">
+        <div className="bg-skeleton-purple p-3 rounded-lg rounded-tr-none max-w-full">
+          Hi there Hi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi there Hi there Hi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi there Hi there Hi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi there Hi there Hi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi there Hi there
+          Hi thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi there Hi there Hi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi there Hi there Hi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi there
+          Hi there Hi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi there
+        </div>
+        <div className="size-7">
+          <CircleUser className="object-cover" />
+        </div>
+      </div>
+      <div className="w-full flex justify-start space-x-2 p-2">
+        <div className="size-7">
+          <CircleUser className="object-cover" />
+        </div>
+        <div className="bg-skeleton-purple p-3 rounded-lg rounded-tl-none max-w-full">
+          Hi there Hi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi there Hi there Hi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi there Hi there Hi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi there Hi there Hi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi there Hi there
+          Hi thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi there Hi there Hi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi there Hi there Hi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi there
+          Hi there Hi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi there
+        </div>
+      </div>
+      <div className="w-full flex justify-start space-x-2 p-2">
+        <div className="size-7">
+          <CircleUser className="object-cover" />
+        </div>
+        <div className="bg-skeleton-purple p-3 rounded-lg rounded-tl-none max-w-full flex justify-center items-center">
+          Hi there Hi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi there Hi there Hi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi there Hi there Hi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi there Hi there Hi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi there Hi there
+          Hi thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi there Hi there Hi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi there Hi there Hi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi there
+          Hi there Hi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi thereHi thereHi thereHi
+          thereHi thereHi thereHi thereHi thereHi there
+        </div>
+      </div>
+      <div className="w-full flex justify-start space-x-2 p-2">
+        <div className="size-7">
+          <CircleUser className="object-cover" />
+        </div>
+        <div className="bg-skeleton-purple p-3 rounded-lg rounded-tl-none max-w-full">
+          Hi there Hi thereHi thereHi there
+        </div>
+      </div>
+      <div className="w-full flex justify-end space-x-2 p-2">
+        <div className="bg-skeleton-purple p-3 rounded-lg rounded-tr-none max-w-full">
+          Hi there Hi thereHi thereHi there
+        </div>
+        <div className="size-7">
+          <CircleUser className="object-cover" />
+        </div>
+      </div>
+      <div className="w-full flex justify-end space-x-2 p-2 pr-10">
+        <div className="bg-skeleton-purple p-3 rounded-lg max-w-full">
+          Hi there Hi thereHi thereHi there
+        </div>
+      </div>
+    </div>
+  );
+};
