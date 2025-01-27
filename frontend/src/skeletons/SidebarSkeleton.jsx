ba88ed1526d0994de4f34a1de677fd5ef5e8ae2b
bharaths1803 <bharaths14051803@gmail.com>
2025-01-28 00:29:44 +0530

    Added sidebar skeleton

--- frontend/src/skeletons/SidebarSkeleton.jsx
+++ frontend/src/skeletons/SidebarSkeleton.jsx
@@ -1,14 +1,14 @@
 const SidebarSkeleton = () => {
-  const dummyArray = new Array(6).fill(null);
+  const dummyArray = new Array(4).fill(null);
   return (
     <div className="space-y-14 pt-2">
       {dummyArray.map((_, idx) => (
         <button
           key={idx}
-          className="w-full flex items-center bg-skeleton-purple rounded-3xl h-10 p-3 space-x-2 animate-pulse"
+          className="w-full flex items-center bg-skeleton-purple rounded-lg h-14 p-3 space-x-2 animate-pulse"
         >
-          <div className="size-5 bg-purple-700 rounded-full" />
-          <div className="w-2/3 h-5 bg-purple-700 rounded-full" />
+          <div className="size-10 bg-purple-700 rounded-full" />
+          <div className="w-2/3 h-5 bg-purple-700" />
         </button>
       ))}
     </div>
